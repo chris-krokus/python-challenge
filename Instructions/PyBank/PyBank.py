cc3f23025783bad5ec7ae39f2f81cf365be1964e
Krokus <Chris.Krokus@gartner.com>
2020-05-24 17:52:34 -0500

    re-do Homework 3

--- Instructions/PyBank/PyBank.py
+++ Instructions/PyBank/PyBank.py
@@ -1,10 +1,8 @@
 #PyBank
 
-
-
 import csv
 
-csvpath = r"C:\Users\ckrokus\Desktop\Education\Homework\03-Python\Instructions\PyBank\Resources/budget_data.csv"
+csvpath = "Resources/budget_data.csv"
 
 with open(csvpath) as csvfile:
 
@@ -21,29 +19,39 @@ with open(csvpath) as csvfile:
     # Total profit
     total_profit = 0
     # Change List 
-    profit_changes = []
-    last_profit = 0
-    current_profit = 0
-    # Row Count
-    row_count = 0
+    total_changes = 0
+    max_change = 0
+    min_change = 0 
     # Read each row of data after the header
     for row in csvreader:
         print(row)
         # The total number of months included in the dataset
         total_months = total_months + 1
-        print(total_months)
+        
         # The net total amount of "Profit/Losses" over the entire period
         total_profit = total_profit + int(row[1])
-        print(total_profit)
+        
         # The average of the changes in "Profit/Losses" over the entire period
         
-        #If row_count == 0 then
-        #last_profit = int(row[1])
-        #else:
-        current_profit = int(row[1])
-        change = current_profit - last_profit        
+        if total_months == 1:
+            last_profit = int(row[1])
+        else:
+            current_profit = int(row[1])
+            change = current_profit - last_profit        
         # Add change to list
-        profit_changes.append(change)
-        last_profit = current_profit 
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest decrease in losses (date and amount) over the entire period
+            total_changes = total_changes + change
+            last_profit = current_profit 
+        #The greatest increase in profits (date and amount) over the entire period
+            if change > max_change:
+                max_change = change
+        
+        #The greatest decrease in losses (date and amount) over the entire period
+            if change < min_change:
+                min_change = change
+print("Financial Analysis")
+print("-------------------------------")
+print(f"Total Months:{total_months}")
+print(f"Total:${total_profit}")
+print(f"Average Change:${total_changes / (total_months-1)}")
+print(f"Greatest Increase in Profits:${max_change}")
+print(f"Greatest Decrease in Profits:${min_change}")
